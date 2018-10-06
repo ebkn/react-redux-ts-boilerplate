6eb09742b2a62e5183ccc7f0d275eb31945a1b71
ebkn <ktennis.mqekr12@gmail.com>
2018-10-06 15:39:29 +0900

    feat: move store.ts to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,29 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import * as thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+let composeWithDevTools: any;
+if (process.env.NODE_ENV !== 'development') {
+  /* tslint:disable-next-line:no-var-requires */
+  composeWithDevTools = require('redux-devtools-extension/logOnlyInProduction').composeWithDevTools;
+} else {
+  /* tslint:disable-next-line:no-var-requires */
+  composeWithDevTools = require('redux-devtools-extension').composeWithDevTools;
+}
+import { RootState } from './state';
+// import reducers here
+// ex) article: ArticleReducer
 import App from './containers/App';
-import store from './store';
+
+const store = createStore(
+  combineReducers<RootState>({}),
+  composeWithDevTools(applyMiddleware(thunk.default)),
+);
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root') as HTMLElement,
+  document.getElementById('root') as HTMLElement
 );
